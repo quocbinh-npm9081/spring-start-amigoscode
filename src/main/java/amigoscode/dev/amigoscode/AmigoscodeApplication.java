be01b48df8a6849424e39b200cbb57b520a91faf
quocbinh-npm9081 <quocbinhgithub@gmail.com>
2022-12-11 20:59:54 +0700

    [add] add student class

--- src/main/java/amigoscode/dev/amigoscode/AmigoscodeApplication.java
+++ src/main/java/amigoscode/dev/amigoscode/AmigoscodeApplication.java
@@ -1,18 +1,21 @@
 package amigoscode.dev.amigoscode;
 
+import amigoscode.dev.amigoscode.Entity.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class AmigoscodeApplication {
 	@GetMapping("/")
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(new Student( 1L, "dfd", 12, LocalDate.of(2000, Month.JANUARY,5), "binh@gmail.com"));
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(AmigoscodeApplication.class, args);
